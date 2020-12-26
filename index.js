6191b31f9234594df09fab959168041416d8d09c
botxboom <kumarjhagulshan@gmail.com>
2020-12-26 09:19:35 +0530

    almost done

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, PubSub } = require('apollo-server')
+const mongoose = require('mongoose')
+
+const { MONGODB } = require("./config.js")
+const typeDefs = require('./graphql/typedefs');
+const resolvers = require('./graphql/resolvers')
+
+const pubsub = new PubSub()
+
+const PORT = process.env.port || 5000
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers, 
+    context: ({ req }) => ({ req, pubsub }) 
+})
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB.')
+        return server.listen({ port: PORT })
+    }).then(res => {
+        console.log(`Server running at ${res.url}` )
+    }).catch(err => {
+        console.log(err)
+    })
